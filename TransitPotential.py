64b3d263117af7074b447db0e4b5d4f68e4792b1
rbjork <grbtxtmsg@gmail.com>
2015-12-16 08:56:46 -0800

    Metro Transit Potential

--- TransitPotential.py
+++ TransitPotential.py
@@ -3,11 +3,23 @@ import os
 from Tkinter import Tk
 from tkFileDialog import askdirectory
 
+def doesFieldExist(fc, fname):
+  lst = arcpy.ListFields(fc)
+  exists = False
+  for f in lst:
+    if f.name == fname:
+      exists = True
+      break
+  return exists
+
 def addTransitAccess(transitdir,countydir,transitdata,geoDataBaseName, addfield, transitstops_lyr):
     countypath = dataPath + countydir + "\\"
     arcpy.FeatureClassToGeodatabase_conversion(transitdir + "\\" + countydir + "\\" + transitdata, countypath + geoDataBaseName)
-    if addfield:
+    featureclass= transitdata.split(".")[0]
+    accessExists = doesFieldExist(countypath + geoDataBaseName + "\\" + featureclass, "access")
+    if not accessExists:
         arcpy.AddField_management(countypath + geoDataBaseName + "\\" + transitdata, "access", "TEXT")
+    '''
     expression = "getJobAccessScore(!X_Coord!)"
     codeblock = """def getJobAccessScore(lng):
     v = int(lng)
@@ -18,8 +30,8 @@ def addTransitAccess(transitdir,countydir,transitdata,geoDataBaseName, addfield,
     if v > 5979000:
         return '3'"""
 
-    featureclass= transitdata.split(".")[0]
-    arcpy.CalculateField_management(countypath + "\\" + geoDataBaseName + "\\" + featureclass, "access", expression,"PYTHON_9.3",codeblock)
+    #arcpy.CalculateField_management(countypath + "\\" + geoDataBaseName + "\\" + featureclass, "access", expression,"PYTHON_9.3",codeblock)
+    '''
     arcpy.MakeFeatureLayer_management(countypath + "\\" + geoDataBaseName + "\\" + featureclass, transitstops_lyr)
 
 def addTransitPotential(countydir,countyparcelnames,geoDataBaseName):
@@ -50,80 +62,88 @@ def features2gdb(countyfoldersList,outdir,geodbname):
         n = n + 1
     return newfilelist
 
+
+
 # Alternative approach which may not be needed - creates seperate features for each set of transit stops distinquished by access
 def splitTransitStopsData(transitdata,transitDataLayer,countyDir):
     featureclass= transitdata.split(".")[0]
     countypath = dataPath + countydir + "\\"
-
     arcpy.MakeFeatureLayer_management(countypath + "\\" + geoDataBaseName + "\\" + featureclass, transitDataLayer)
-    arcpy.SelectLayerByAttribute_management(transitDataLayer, "NEW_SELECTION", "access > 0")
+    arcpy.SelectLayerByAttribute_management(transitDataLayer, "NEW_SELECTION", "\"access\" = 1")
     arcpy.CopyFeatures_management(transitDataLayer, dataPath + countyDir + "\\" + geoDataBaseName + "\\transitStopsAccessLow")
-    arcpy.SelectLayerByAttribute_management(transitDataLayer, "NEW_SELECTION", "access > 1")
+    arcpy.SelectLayerByAttribute_management(transitDataLayer, "NEW_SELECTION", "\"access\" = 2")
     arcpy.CopyFeatures_management(transitDataLayer, dataPath + countyDir + "\\" + geoDataBaseName + "\\transitStopsAccessMedium")
-    arcpy.SelectLayerByAttribute_management(transitDataLayer, "NEW_SELECTION", "access > 2")
+    arcpy.SelectLayerByAttribute_management(transitDataLayer, "NEW_SELECTION", "\"access\" = 3")
     arcpy.CopyFeatures_management(transitDataLayer, dataPath + countyDir + "\\" + geoDataBaseName + "\\transitStopsAccessHigh")
-
 
 def bufferTransitStops_QUARTER(transitstops, bufferedName, geoDataBaseName):
     print "bufferDestinations"
     countypath = dataPath +  countydir + "\\"
-    arcpy.Buffer_analysis( countypath  + geoDataBaseName + "\\" + transitstops, transitdir + "\\" + geoDataBaseName + "\\" + bufferedName, "2640 Feet")
+    arcpy.Buffer_analysis( countypath  + geoDataBaseName + "\\" + transitstops, countypath + "\\" + geoDataBaseName + "\\" + bufferedName, "1320 Feet")
 
 def bufferTransitStops_HALF(transitstops, bufferedName, geoDataBaseName):
     print "bufferDestinations"
     countypath = dataPath +  countydir + "\\"
-    arcpy.Buffer_analysis( countypath  + geoDataBaseName + "\\" + transitstops, transitdir + "\\" + geoDataBaseName + "\\" + bufferedName, "2640 Feet")
+    arcpy.Buffer_analysis( countypath  + geoDataBaseName + "\\" + transitstops, countypath + "\\" + geoDataBaseName + "\\" + bufferedName, "2640 Feet")
 
-def scoreparcels(targetFeature,bufferedTransitStop,geoDataBaseName, quarterMile, access):
-    fcnear = bufferedTransitStop + "_shp" #'c:/data/base.gdb/well'
-    fctarget = targetFeature + "_shp"
-    arcpy.env.workspace = homedir + "\\" + geoDataBaseName
-    arcpy.MakeFeatureLayer_management(targetFeature, 'parcels_lyr')
+def scoreparcels(targetFeature,bufferedTransitStop,geoDataBaseName, halfMile, access, layerCount):
+    layerCount = layerCount + 1
+    parcel_layer = 'parcels_lyr' + str(layerCount)
+    fcnear = bufferedTransitStop  #'c:/data/base.gdb/well'
+    fctarget = targetFeature
+    countyPath = dataPath + countydir + "\\"
+    arcpy.env.workspace = countyPath + "\\" + geoDataBaseName
+    arcpy.MakeFeatureLayer_management(targetFeature, parcel_layer)
     #arcpy.SelectLayerByAttribute_management(fcnear, "SUBSET_SELECTION", "access > 10000")
-    arcpy.SelectLayerByLocation_management(fcnear, 'intersect', fctarget)
-    expression = "setScore("+str(access)+")"
-    if quarterMile:     # Quarter Mile
+    arcpy.SelectLayerByLocation_management(parcel_layer, 'intersect', fcnear)
+    #arcpy.CopyFeatures_management('parcels_lyr', countyPath + "\\" + geoDataBaseName + "\\?")
+    expression = "setScore('"+str(access)+"')"
+    if halfMile:     # Quarter Mile
         codeblock = """def setScore(access):
         if access == 'LOW':
             return '1'
         if access == 'MEDIUM':
-            return '2'
+            return '3'
         if access == 'HIGH':
-            return '3'"""
+            return '5'"""
     else:               # Half Mile
-        codeblock = """def getScore(access):
+        codeblock = """def setScore(access):
         if access == 'LOW':
-            return '4'
+            return '2'
         if access == 'MEDIUM':
-            return '5'
+            return '4'
         if access == 'HIGH':
             return '6'"""
-    arcpy.CalculateField_management(transitdir + "\\" + geoDataBaseName + "\\transitsStops", "access", expression,"PYTHON_9.3",codeblock)
+
+    countypath = dataPath +  countydir + "\\"
+    arcpy.CalculateField_management(parcel_layer, "potential", expression,"PYTHON_9.3",codeblock)
 
 
 # No Split required
-def scoreparcels2(targetFeature,bufferedTransitStop,geoDataBaseName, quarterMile, access):
+def scoreparcels2(targetFeature,bufferedTransitStop,geoDataBaseName, halfMile, access, layerCount):
+    layerCount = layerCount + 1
+    parcel_layer = 'parcels_lyr' + str(layerCount)
     fcnear = bufferedTransitStop + "_shp" #'c:/data/base.gdb/well'
     fctarget = targetFeature + "_shp"
     arcpy.env.workspace = homedir + "\\" + geoDataBaseName
-    arcpy.MakeFeatureLayer_management(targetFeature, 'parcels_lyr')
+    arcpy.MakeFeatureLayer_management(targetFeature, parcel_layer)
     arcpy.SelectLayerByAttribute_management(fcnear, "NEW_SELECTION", "access > " + str(access))
     arcpy.SelectLayerByLocation_management(fcnear, 'intersect', fctarget)
-    expression = "setScore("+str(access)+")"
-    if quarterMile:     # Quarter Mile
+    expression = "setScore('"+str(access)+"')"
+    if halfMile:     # Quarter Mile
         codeblock = """def setScore(access):
         if access == 'LOW':
             return '1'
         if access == 'MEDIUM':
-            return '2'
+            return '3'
         if access == 'HIGH':
-            return '3'"""
+            return '5'"""
     else:               # Half Mile
         codeblock = """def getScore(access):
         if access == 'LOW':
-            return '4'
+            return '2'
         if access == 'MEDIUM':
-            return '5'
+            return '4'
         if access == 'HIGH':
             return '6'"""
     arcpy.CalculateField_management(transitdir + "\\" + geoDataBaseName + "\\transitsStops", "access", expression,"PYTHON_9.3",codeblock)
@@ -132,11 +152,14 @@ def scoreparcels2(targetFeature,bufferedTransitStop,geoDataBaseName, quarterMile
 '''
 There are two groups of datasets: Parcels and TransitStops.
 Within TransitStops datasets there are three smaller datasets: transitStopsAccessLow,transitStopsAccessMedium and transitStopsAccessHigh
-
 '''
 
+
+
+
+
 def promptForFoldersForMetro():
-    print("Please select the directory containing the metro .ZIP files to be processed:")
+    print("Please select the directory containing the metro counties to be processed:")
     Tk().withdraw() #prevents Tk window from showing up unnecessarily
     file_directory = askdirectory() #opens directory selection dialog
     os.chdir(homedir)
@@ -144,45 +167,59 @@ def promptForFoldersForMetro():
     metro_counties = os.listdir(file_directory)
     return metro_counties
 
+# def createTransitStops(countyFolderList):
+#      for countydir in countyFolderList:
+#         addTransitAccess(transitdir,countydir,transitdata,geoDataBaseName,True,transitLayer)
+#         newgdbfiles = features2gdb(countyFolderList, dataPath + countydir, geoDataBaseName)
+#         addTransitPotential(countydir,newgdbfiles,geoDataBaseName) # sets all parcel TransitPotential values to default value of 0
+#         method1 = True
+#         if method1:
+#             splitTransitStopsData(transitdata,transitLayer,countydir)
+#
+
+
 if __name__ == '__main__':
     homedir =  os.getcwd()
     homePath = homedir + "\\"
     dataPath = homePath + "data\\"
-    countyFolderList = promptForFoldersForMetro()
-    countydir = "06041" #countyFolderList[0] # single county test
-
     transitdir = homedir + "\\transit"
-    transitdata = "GGBRIDGE_transit_stop.shp"
+    transitdata = "transit_stops.shp"
     transitLayer = "transitstops_lyr"
 
-    geoDataBaseName = "transitdb.gdb"
-    '''
-    arcpy.CreateFileGDB_management(dataPath + countydir, geoDataBaseName)
+    countyFolderList = promptForFoldersForMetro()
 
-    # addTransitAccess(transitdir,countydir,transitdata,geoDataBaseName, addfield, transitstops_lyr):
-    addTransitAccess(transitdir,countydir,transitdata,geoDataBaseName,False,transitLayer)
-    newgdbfiles = features2gdb(countyFolderList, dataPath + countydir, geoDataBaseName)
-    addTransitPotential(countydir,newgdbfiles,geoDataBaseName) # sets all parcel TransitPotential values to default value of 0
-    '''
-    method1 = True
-    if method1:
-        splitTransitStopsData(transitdata,transitLayer,countydir)
+    #createTransitStops(countyFolderList)
 
-    '''
-    # Access Low
-    bufferTransitStops_HALF("transitStopsAccessLow","transitbuffered", geoDataBaseName)
-    scoreparcels("parcels","transitbuffered", "transitPotential", geoDataBaseName,False,0) # Give overlaps parcels score 1
-    bufferTransitStops_QUARTER("transitStopsAccessLow","transitbuffered", geoDataBaseName)
-    scoreparcels("parcels","transitbuffered", "transitPotential", geoDataBaseName,True,0) # Give overlaps parcels score 2
-    # Access Medium
-    bufferTransitStops_HALF("transitStopsAccessMedium","transitbuffered", geoDataBaseName)
-    scoreparcels("parcels","transitbuffered", "transitPotential", geoDataBaseName,False,1) # Give overlaps parcels score 3
-    bufferTransitStops_QUARTER("transitStopsAccessMedium","transitbuffered", geoDataBaseName)
-    scoreparcels("parcels","transitbuffered", "transitPotential", geoDataBaseName,True,1) # Give overlaps parcels score 4
-    # Access High
-    bufferTransitStops_HALF("transitStopsAccessHigh","transitbuffered", geoDataBaseName)
-    scoreparcels("parcels","transitbuffered", "transitPotential", geoDataBaseName,False,2) # Give overlaps parcels score 5
-    bufferTransitStops_QUARTER("transitStopsAccessHigh","transitbuffered", geoDataBaseName)
-    scoreparcels("parcels","transitbuffered", "transitPotential", geoDataBaseName,True,2) # Give overlaps parcels score 6
-    '''
+    for countydir in countyFolderList:
+        #countydir = "06041" #countyFolderList[0] # single county test
+        geoDataBaseName = "transitdb"  + countydir + ".gdb"
+        arcpy.CreateFileGDB_management(dataPath + countydir, geoDataBaseName)
+        #addTransitAccess(transitdir,countydir,transitdata,geoDataBaseName, addfield, transitstops_lyr):
+        addTransitAccess(transitdir,countydir,transitdata,geoDataBaseName,True,transitLayer)
+        newgdbfiles = features2gdb(countyFolderList, dataPath + countydir, geoDataBaseName)
+        addTransitPotential(countydir,newgdbfiles,geoDataBaseName) # sets all parcel TransitPotential values to default value of 0
 
+        method1 = True
+        if method1:
+            splitTransitStopsData(transitdata,transitLayer,countydir)
+
+        layerCount = 0
+
+        # Low Access
+        bufferTransitStops_HALF("transitStopsAccessLow","transitbufferedLow1", geoDataBaseName)
+        scoreparcels("parcels","transitbufferedLow1",  geoDataBaseName,True,'LOW', layerCount) # Give overlaps parcels score 1
+        bufferTransitStops_QUARTER("transitStopsAccessLow","transitbufferedLow2", geoDataBaseName)
+        scoreparcels("parcels","transitbufferedLow2",  geoDataBaseName,False,'LOW', layerCount) # Give overlaps parcels score 2
+        # Medium Access
+        bufferTransitStops_HALF("transitStopsAccessMedium","transitbufferedMedium1", geoDataBaseName)
+        scoreparcels("parcels","transitbufferedMedium1",  geoDataBaseName,True,'MEDIUM', layerCount) # Give overlaps parcels score 3
+        bufferTransitStops_QUARTER("transitStopsAccessMedium","transitbufferedMedium2", geoDataBaseName)
+        scoreparcels("parcels","transitbufferedMedium2",  geoDataBaseName,False,'MEDIUM', layerCount) # Give overlaps parcels score 4
+        # High  Access
+        bufferTransitStops_HALF("transitStopsAccessHigh","transitbufferedHigh1", geoDataBaseName)
+        scoreparcels("parcels","transitbufferedHigh1",  geoDataBaseName,True,'HIGH',layerCount) # Give overlaps parcels score 5
+        bufferTransitStops_QUARTER("transitStopsAccessHigh","transitbufferedHigh2", geoDataBaseName)
+        scoreparcels("parcels","transitbufferedHigh2",  geoDataBaseName,False,'HIGH',layerCount) # Give overlaps parcels score 6
+
+
+    print "COMPLETE"
